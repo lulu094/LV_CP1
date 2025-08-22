98eb2aa63426e87654d4f8ebd5d5c033b6792c4e
lulu094 <lucci.venanciomostaceli@ucas-edu.net>
2025-08-22 08:29:30 -0600

    starter about variables

---
+++ notes/variables.py
@@ -0,0 +1,10 @@
+## LV 2nd Variables Notes
+
+students_count = 29
+amount_water = "1oz"
+name = "Jake"
+grade = 98
+print(grade)
+
+grade = 88
+print(grade)
---
+++ practices/Starters/season.py
@@ -0,0 +1,4 @@
+# LV 2nd Season
+name=input("What is your last name:")
+season=input("What's your favorite season:")
+print=("Wow",)
