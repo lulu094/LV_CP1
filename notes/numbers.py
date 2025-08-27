b2589e6539769a489df858c56f6d3fb040a9b4db
lulu094 <lucci.venanciomostaceli@ucas-edu.net>
2025-08-27 10:50:48 -0600

    integers and floats math problem

---
+++ notes/numbers.py
@@ -0,0 +1,15 @@
+# LV 2nd Integers and Floats Notes
+#Integers
+age = 65496499999999999999999999
+temp = 85
+attention = -8
+spoons = 4
+#Float (Floating Point Number)
+pi = 3.14
+pounds = 5.6
+emotional_damage = .99
+patience_level = -.50
+
+
+#MATH
+print(8/4 + 7 - (8+2)%4**2)
