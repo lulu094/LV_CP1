d64ea5f4a79ae9131d3246cc0d435743aa491500
lulu094 <lucci.venanciomostaceli@ucas-edu.net>
2025-08-28 10:25:45 -0600

    change data types, and rounding numbers

--- notes/numbers.py
+++ notes/numbers.py
@@ -13,3 +13,17 @@ patience_level = -.50
 
 #MATH
 print(8/4 + 7 - (8+2)%4**2)
+
+#Change data types
+age = int(input("How old are you?\n"))
+
+
+print("Wow that is old! That is", 525600*age, "minutes!")
+
+brain__cells = float(input("How many brain cells does your class have? (Example: .3)\n"))
+
+print("Are you sure it isn't", brain__cells/2, "?")
+
+#Round
+pi = 3.14149265358979
+print(round(pi, 4))
