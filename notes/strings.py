76bdf36331b0fbd9ed50e79e1c17b88ce58ba400
lulu094 <lucci.venanciomostaceli@ucas-edu.net>
2025-09-02 10:51:17 -0600

    learned about slice, escape character

--- notes/strings.py
+++ notes/strings.py
@@ -1,1 +1,34 @@
 # LV 2nd Strings Notes
+
+#Examples
+sentence ="The quick brown fox jumps over the lazt dog!"
+first_name = 'Xavier'
+month = 'September'
+food = "chocolate"
+last_name = "LaRose"
+
+#If I need to use ' or "
+name = "La'Rose"
+best = '"The Return of the King" is the best book ever!'
+
+
+funny = "The dog jumped into the lake"
+
+#length og strings
+length = len(month)
+print("September has", length, "letters")
+
+#Escape Character
+print('\tWill said: \n\t\t\t "Don\'t die"')
+
+#Concatenate
+full_name =  first_name + " " + last_name
+
+print(full_name)
+
+#Index
+
+
+#Slice
+
+print(last_name[2:6])
