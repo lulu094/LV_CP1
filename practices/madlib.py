2053d0d7ff9cc612c5ee307995b4f3bfc5eae9f8
lulu094 <lucci.venanciomostaceli@ucas-edu.net>
2025-09-03 10:45:56 -0600

    learned to do a madlib in vs

---
+++ practices/madlib.py
@@ -0,0 +1,12 @@
+# LV 2nd Madlib
+noun = input("Enter a noun:")
+verb= input("Enter a verb that you would do outdoors:")
+adjective = input("Enter an adjective:")
+place = input("Enter a place:")
+animal = input("Enter an animal:")
+
+print("One day, a " + adjective + " " + noun + " decided to " + verb + " all the way to " + place + ".")
+print("Along the way,he saw a  " + animal + " dancing on a rock.")
+print("The " + noun + " couldn't believe its eyes, so he satarted to " + verb + " faster.")
+print("When he finally got to " + place + " , he was completely out of breath.")
+print("He sat down next to the " + animal + " and they became best friends forever.")
