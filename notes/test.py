64b994ca46aa4dea0034671011446d72260fe5f4
lulu094 <lucci.venanciomostaceli@ucas-edu.net>
2025-09-09 16:26:25 +0000

    code in codespaces

--- notes/test.py
+++ notes/test.py
@@ -1,4 +1,5 @@
 print("Test")
 print("Uptdate")
 print("Practice sync and homework")
+print("I added this on codespaces. That place where I can do homework")
 #print () <= Everything inside the parenthesis apears in the terminal
