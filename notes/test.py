0b6b2335d797d68401cd2dd4baffc3e3f7b55fce
lulu094 <lucci.venanciomostaceli@ucas-edu.net>
2025-09-17 10:43:15 -0600

    checked my code

--- notes/test.py
+++ notes/test.py
@@ -2,4 +2,5 @@ print("Test")
 print("Uptdate")
 print("Practice sync and homework")
 print("I added this on codespaces. That place where I can do homework")
+print("New seat 9/17")
 #print () <= Everything inside the parenthesis apears in the terminal
