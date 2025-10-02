3272dd2af41b79502dfd0344a873cd332bd5d450
lulu094 <lucci.venanciomostaceli@ucas-edu.net>
2025-10-02 04:44:57 +0000

    started the code, still have to figure out how to finish it

--- practices/multiplication_table.py
+++ practices/multiplication_table.py
@@ -1,2 +1,7 @@
 # LV 2nd Multiplication Table
-num = [1,2,3,4,5,6,7,8,9,10,11,12]
+
+#Print header row (numbers 1 to 12)
+print("    ", end= "")#Spacing for the top-left corner
+for i in range(1,13):#Loop through numbers 1 to 12
+    print(f"{i:4}", end= "") #Print the too header numbers with spacing
+print()#Move to a new line
