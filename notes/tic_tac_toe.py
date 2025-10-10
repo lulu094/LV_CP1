cc5bd5fd01a56f9d60b9cc7f19667d7856b8d613
lulu094 <lucci.venanciomostaceli@ucas-edu.net>
2025-10-10 10:23:46 -0600

    starter group project

---
+++ notes/tic_tac_toe.py
@@ -0,0 +1,19 @@
+
+#computer "O"
+#player "X"
+
+#elif player (1, "X") and player (4, "X") and player (7,"X"):
+   # print("You win!")
+#elif player (2, "X") and player (5, "X") and player (8,"X"):
+    #print("You win!")
+#elif player (3, "X") and player (6, "X") and player (9,"X"):
+   # print("You win!")
+
+#elif computer (1, "O") and computer (2, "O") and computer (3,"O"):
+   # print("Computer wins!")
+
+#elif computer (4, "O") and computer (5, "O") and computer (6,"O"):
+    #print("Computer wins!")
+
+#elif computer (7, "O") and computer (8, "O") and computer (9,"O"):
+   # print("Computer wins!")
