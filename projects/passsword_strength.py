8408c2dcf1f70a3e9f508fff06424aee125604a8
lulu094 <lucci.venanciomostaceli@ucas-edu.net>
2025-10-13 10:50:43 -0600

    en proceso de terminar el proyecto. espremos terminarlo manana

--- projects/passsword_strength.py
+++ projects/passsword_strength.py
@@ -29,25 +29,46 @@ if len(password) >= 8:
     score += 1         # Add 1 point to the password strength score
 # Check if password contains at least one uppercase letter
 # This helps make passwords more complex and secure
-
+if any( char.isupper() for char in password):
+    uppercase = True
+    score += 1
 # Check if password contains at least one lowercase letter
 # Required to ensure the password isn’t all uppercase or numbers
-
+if any( char.islower() for char in password):
+    lowercase = True
+    score += 1
 # Check if password contains at least one number
 # Including digits adds variety and strength to the password
-
+if any( char.isdigit() for char in password):
+    numbers = True
+    score += 1
 # Check if password contains at least one special character
 # Special characters increase password complexity and reduce the chance of it being cracked
-
+if any( char in special_characters for char in password):
+    special = True
+    score += 1
 # Output results of checks
 
 # Show a checklist of what the password includes
 # This helps the user understand which rules they met and which ones they missed
-
+print("\n Password strength:")
+print(f"Length ")
+print(f"")
+print(f"")
+print(f"")
+print(f"")
+print(f"")
 # Display final strength score
 
 # Show the user how many points out of 5 their password earned
 
-
 # Give final feedback based on score
 # Use if/elif statements to give a final message depending on the total score
+if score <= 2:
+    print(f"You have a weak password")
+elif score == 3:
+    print(f"You have a moderate password")
+elif score == 4:
+    print(f"You have a strong password")
+else:
+    print(f"You have a very strong password!")
