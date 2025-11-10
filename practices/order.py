3b94755556054c69d7d5341ea7f0f47060436398
lulu094 <lucci.venanciomostaceli@ucas-edu.net>
2025-11-10 10:50:53 -0700

    doing pseudocde

---
+++ practices/order.py
@@ -0,0 +1,24 @@
+# LV 2nd Order Up
+#make a library for drinks
+    # cost
+    # name of the drinks and rates
+    # print the cost and drink ordered
+#make a library for appetizers
+    # cost
+    # name of the appetizers and rates
+    # print the cost and appetizers ordered
+#make a library for soup
+    # cost
+    # name of the soup and rates
+    # print the cost and soup ordered
+#make a library for special meals
+    # cost
+    # name of the special meals and rates
+    # print the cost and special meals ordered
+#make a library for chicken & Steak
+    # cost
+    # name of the appetizers and rates
+    # print the cost and appetizers ordered
+drink = {
+
+}
