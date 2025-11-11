ec82b3c7153dc890bb5cd413d85c2de8ec2a8ff2
lulu094 <lucci.venanciomostaceli@ucas-edu.net>
2025-11-11 10:50:28 -0700

    termine haciendo dictionaries

--- practices/order.py
+++ practices/order.py
@@ -1,24 +1,48 @@
 # LV 2nd Order Up
-#make a library for drinks
+#make a library for drinks,appetizers,soup,special meals, seafood, and desserts
     # cost
     # name of the drinks and rates
     # print the cost and drink ordered
-#make a library for appetizers
-    # cost
-    # name of the appetizers and rates
-    # print the cost and appetizers ordered
-#make a library for soup
-    # cost
-    # name of the soup and rates
-    # print the cost and soup ordered
-#make a library for special meals
-    # cost
-    # name of the special meals and rates
-    # print the cost and special meals ordered
-#make a library for chicken & Steak
-    # cost
-    # name of the appetizers and rates
-    # print the cost and appetizers ordered
-drink = {
 
+
+print("Welcome to Puro Peru!")
+
+drinks = {
+    "Inca Kola": 4.81,
+    "Coca Cola ": 3.59,
+    "Chicha Morada": 3.50,
+    "Sprite": 2.99
 }
+
+appetizer = {
+    "Causa Rellena": 18.99,
+    "Tamal Peruano": 8.99,
+    "Anticucho Tradicional": 9.99,
+    "Papa rellena": 18.99
+}
+
+soups = {
+    "Sopa Criolla": 10.99,
+    "Sopa de Paico": 12.99,
+    "Cazuela":
+    "Chupe":
+}
+
+special_meals = {
+    "Desayuno Lurin": 17.00,
+    "Chicharron de chancho": 12.99,
+    "Chicharron de pollo": 12.99
+}
+
+seafood = {
+    "Pescado a lo macho": 19.99,
+    "Saltado de camarones": 15.99,
+    "Tallarines saltados con mariscos" : 19.99,
+    "Tallarines verdes con mariscos": 19.99
+}
+
+dessert = {
+    "Alfajor Clasico": 5.99,
+    "Pionono Peruano": 8.99
+}
+
