eab4dba62bc7463ef76600518b3ce4cefaaee325
lulu094 <lucci.venanciomostaceli@ucas-edu.net>
2025-11-12 04:42:38 +0000

    avanze poquito

--- practices/order.py
+++ practices/order.py
@@ -24,14 +24,15 @@ appetizer = {
 soups = {
     "Sopa Criolla": 10.99,
     "Sopa de Paico": 12.99,
-    "Cazuela":
-    "Chupe":
+    "Cazuela":6.99,
+    "Chupe":10.99
 }
 
 special_meals = {
     "Desayuno Lurin": 17.00,
     "Chicharron de chancho": 12.99,
-    "Chicharron de pollo": 12.99
+    "Chicharron de pollo": 12.99,
+    "Chicharron de pescado": 12.99
 }
 
 seafood = {
@@ -43,6 +44,40 @@ seafood = {
 
 dessert = {
     "Alfajor Clasico": 5.99,
-    "Pionono Peruano": 8.99
+    "Pionono Peruano": 8.99,
+    "Arroz con leche": 5.99,
+    "Masamorra morada": 5.99
 }
 
+# Start order
+total = 0
+order_drink = ""
+order_main = ""
+order_sides = []
+
+# Choose drink
+#print("\nDrinks:")
+#for d in drinks:
+    #print(d)
+#while True:
+    #order_drink = input("Choose a drink: ")
+    #if order_drink in drinks:
+       # total += drinks[order_drink]
+       # break
+    #else:
+       # print("Sorry, that is not on the menu. Try again.")
+
+# Choose main course
+#print("\nMain Courses:")
+#main_courses = {}
+#main_courses.update(special_meals)
+#main_courses.update(seafood)
+#for m in main_courses:
+    #print(m)
+#while True:
+    #order_main = input("Choose a main course: ")
+    #if order_main in main_courses:
+    #    total += main_courses[order_main]
+    #    break
+    #else:
+    #    print("Sorry, that is not on the menu. Try again.")
