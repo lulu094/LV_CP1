60af9199c8194b7b2c0ee9ce55acbf36fc4015d5
lulu094 <lucci.venanciomostaceli@ucas-edu.net>
2025-11-24 03:15:15 +0000

    emmas pseudocode

--- practices/factorial_calc.py
+++ practices/factorial_calc.py
@@ -1,23 +1,48 @@
 # LV 2nd Factorial Calculator
 # Welcome the user
-print("Welcome to Factorial Calculator!!!")
 # User inputs
 #Use int( ) or str() to make sure they use numbers
-og= str(("Enter a number:"))
 # Use if statements to make sure it is true
-if og("integer")
-    print()
-else:
-    print("Please type a number")
 # else say its an invalid input, make user.type a number continue the code
 #ask user if it wants to put another number
-multiply by them in a function
 # (original # * og#-1, og#-2 till you get to one) = answer
 # print out the numbers inputed by the User
-print("do you want to continue using the calculator (yes/done)?")
 #when user types "done"
-    if user(yes):
-        print()
-    
-    
 #print ( thank you for using the calculator)
+
+print("Welcome to the LV 2nd Factorial Calculator!")   
+# Welcome the user
+# (already done above)
+
+# User inputs
+while True:                                            
+    user_input = input("Enter a number to calculate its factorial (or type 'done' to exit): ")
+
+#Use int( ) or str() to make sure they use numbers
+    # actual code checks input below
+
+# Use if statements to make sure it is true
+    if user_input.lower() == "done":                   
+        break
+
+# else say its an invalid input, make user.type a number continue the code
+    if not user_input.isdigit():                       
+        print("Invalid input. Please type a number.")
+        continue
+
+#ask user if it wants to put another number
+    num = int(user_input)                              
+    factorial = 1
+
+# (original # * og#-1, og#-2 till you get to one) = answer
+    while num > 1:                                    
+        factorial *= num
+        num -= 1
+
+# print out the numbers inputed by the User
+    print(f"The factorial of {original} is: {factorial}")  
+#when user types "done"
+# (handled above)
+
+#print ( thank you for using the calculator)
+print("Thank you for using the calculator!")          
